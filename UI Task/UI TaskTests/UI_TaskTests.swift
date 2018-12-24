60e00c7f36010cee94b99e8c625c5befa03a758e
AlexVorobiev <ansfargo@gmail.com>
2018-12-24 14:53:51 +0200

    start

---
+++ UI Task/UI TaskTests/UI_TaskTests.swift
@@ -0,0 +1,34 @@
+//
+//  UI_TaskTests.swift
+//  UI TaskTests
+//
+//  Created by Alex Vorobiev on 12/24/18.
+//  Copyright © 2018 Student. All rights reserved.
+//
+
+import XCTest
+@testable import UI_Task
+
+class UI_TaskTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
