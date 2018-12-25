a3a250c4d6805f3719df3c9cac244e1760c844bd
AlexVorobiev <ansfargo@gmail.com>
2018-12-25 13:05:11 +0200

    add ViewSquare

--- UI Task/UI Task/ViewController.swift
+++ UI Task/UI Task/ViewController.swift
@@ -2,19 +2,24 @@
 //  ViewController.swift
 //  UI Task
 //
-//  Created by Alex Vorobiev on 12/24/18.
+//  Created by Alex Vorobiev on 12/25/18.
 //  Copyright © 2018 Student. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        let label = UILabel(frame: CGRect.init(x: 20, y: 20, width: 90, height: 90))
+//        label.text = "LABEL"
+//        label.textAlignment = NSTextAlignment.center
+//        label.backgroundColor = UIColor.cyan
+//        self.view.addSubview(label)
     }
 
-
+    @IBAction func buttonClick(_ sender: UIButton) {
+    
+    }
 }
-
---
+++ UI Task/UI Task/ViewSquare.swift
@@ -0,0 +1,14 @@
+//
+//  ViewSquare.swift
+//  UI Task
+//
+//  Created by Alex Vorobiev on 12/25/18.
+//  Copyright © 2018 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewSquare: UIView {
+
+
+}
